2dfe2f638c50bfc2ddca7726ce9ef1446b255471
jx-nin <jaszczaninmarceli@gmail.com>
2026-09-05 17:37:44 +0200

    lifecycle management server

--- services/gateway/cmd/gateway/main.go
+++ services/gateway/cmd/gateway/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jx-nin/sretail/services/gateway/internal/router"
 )
@@ -9,7 +16,47 @@ import (
 func main() {
 	r := router.New()
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal(err)
+	s := &http.Server{
+		Addr:              ":8080",
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+		IdleTimeout:       60 * time.Second,
+		MaxHeaderBytes:    1 << 20, // 1 MiB
+	}
+
+	serverErrors := make(chan error, 1)
+
+	shutdownSignal, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	go func() {
+		serverErrors <- s.ListenAndServe()
+	}()
+
+	select {
+	case err := <-serverErrors:
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server failed: %v", err)
+		}
+
+	case <-shutdownSignal.Done():
+		stop()
+		shutdownContext, cancel := context.WithTimeout(
+			context.Background(),
+			10*time.Second,
+		)
+
+		defer cancel()
+		if err := s.Shutdown(shutdownContext); err != nil {
+			log.Printf("graceful shutdown failed: %v", err)
+
+			if closeErr := s.Close(); closeErr != nil {
+				log.Printf("force close failed: %v", closeErr)
+			}
+		}
 	}
 }
